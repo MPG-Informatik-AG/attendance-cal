0fa86e307a00616b19784913b16b3325d7ca6af6
Marc Binder <bindermarc@gmx.de>
2022-12-01 11:00:21 +0100

    added endpoint and ui to see users

--- client/main.js
+++ client/main.js
@@ -1,23 +1,61 @@
+let loginhash = null
+
 window.onload = () => {
-    let loginhash = getCookie("loginhash")
-    if (!loginhash) {
-        window.location = "./login"
-        return
+	loginhash = getCookie("loginhash")
+	if (!loginhash) {
+		window.location = "./login"
+		return
+	}
+	getAllPersons()
+}
+
+function getAllPersons() {
+    var xhr = new XMLHttpRequest()
+    xhr.open("POST", "http://localhost:6677/get/persons", false)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.send(JSON.stringify({
+        loginhash: loginhash
+    }))
+    if (xhr.status == 200) {
+		document.querySelectorAll("#person-list .person").forEach(e => e.remove())
+
+		let persons = JSON.parse(xhr.response)
+		for (let p of persons) {
+			document.getElementById("person-list").innerHTML += `<div class="person">- ${p.name} - ${p.id}</div>`
+		}
+    } else {
+        console.error(xhr.response)
+    }
+}
+
+function createPerson() {
+	console.log("asd")
+	var xhr = new XMLHttpRequest()
+    xhr.open("POST", "http://localhost:6677/new/person", false)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.send(JSON.stringify({
+        loginhash: loginhash,
+		name: document.getElementById("new-person-name").value
+    }))
+    if (xhr.status == 200) {
+		getAllPersons()
+    } else {
+        console.error(xhr.response)
     }
 }
 
 function getCookie(cname) {
-    let name = cname + "="
-    let decodedCookie = decodeURIComponent(document.cookie)
-    let ca = decodedCookie.split(';')
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i]
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1)
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length)
-      }
-    }
-    return undefined
+	let name = cname + "="
+	let decodedCookie = decodeURIComponent(document.cookie)
+	let ca = decodedCookie.split(';')
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i]
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1)
+	}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length)
+		}
+	}
+	return undefined
   }
--- index.js
+++ index.js
@@ -6,21 +6,61 @@ const app = express()
 app.use(body_parser.json({limit: "50mb"}))
 app.use("/", express.static("client"))
 
+//// temp db
+var persons = [
+   {
+      name: "tetsname",
+      id: 99999
+   }
+]
+var person_id = 0
+
 app.get("/a/b", (req, res) => {
    res.send("du bist bei /a/b")
 })
 
 app.post("/login", (req, res) => {
    console.log(req.body)
-   if (sha.sha512(req.body.loginhash) == "2d04221f85f619cd814ecf31bf0ffb3c41969da7497a20105b933eb8b060e0f9a2bfe4f061c9489e87c4959ac945b1fe5306d9622a99b50067d00c84c40900c8") {
+   if(check_hash(req.body.loginhash)) {
       res.send("ok")
    } else {
       res.status(401).send("login failed")
    }
 })
 
+/*
+{
+   loginhash: "",
+   name: "",
+}
+*/
+app.post("/new/person", (req, res) => {
+   if (!check_hash(req.body.loginhash)) {
+      return res.status(401).send("wrong login hash")
+   }
+   persons.push({
+      name: req.body.name,
+      id: person_id
+   })
+   person_id++
+   res.send("ok")
+})
+
+app.post("/get/persons", (req, res) => {
+   if (!check_hash(req.body.loginhash)) {
+      return res.status(401).send("wrong login hash")
+   }
+   res.send(JSON.stringify(persons))
+})
+
 app.listen(6677, () => {
    console.log("[express] server listening")
 })
 
-//some comment
+function check_hash(hash) {
+   if (sha.sha512(hash) == "2d04221f85f619cd814ecf31bf0ffb3c41969da7497a20105b933eb8b060e0f9a2bfe4f061c9489e87c4959ac945b1fe5306d9622a99b50067d00c84c40900c8") {
+      return true
+   } else {
+      return false
+   }
+}
